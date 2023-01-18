8b74e38dbe322b34ba29473825682a3ce9c3ac70
yuehernkang <yuehernkang@hotmail.com>
2023-01-18 10:14:56 +0800

    Initial Commit

--- src/main/java/com/myorg/ExampleProjectApp.java
+++ src/main/java/com/myorg/ExampleProjectApp.java
@@ -4,8 +4,6 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class ExampleProjectApp {
     public static void main(final String[] args) {
         App app = new App();
@@ -17,21 +15,17 @@ public class ExampleProjectApp {
 
                 // Uncomment the next block to specialize this stack for the AWS Account
                 // and Region that are implied by the current CLI configuration.
-                /*
                 .env(Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
                         .region(System.getenv("CDK_DEFAULT_REGION"))
                         .build())
-                */
 
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
-                /*
                 .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
+                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                        .region("ap-southeast-1")
                         .build())
-                */
 
                 // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                 .build());
--- src/main/java/com/myorg/ExampleProjectStack.java
+++ src/main/java/com/myorg/ExampleProjectStack.java
@@ -1,10 +1,25 @@
 package com.myorg;
 
 import software.constructs.Construct;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.appsync.DynamoDbDataSource;
+import software.amazon.awscdk.services.appsync.GraphqlApi;
+import software.amazon.awscdk.services.appsync.SchemaFile;
+import software.amazon.awscdk.services.cognito.CfnIdentityPool;
+import software.amazon.awscdk.services.cognito.CfnUserPool;
+import software.amazon.awscdk.services.cognito.CfnUserPoolGroup;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.s3.Bucket;
 
 public class ExampleProjectStack extends Stack {
     public ExampleProjectStack(final Construct scope, final String id) {
@@ -14,11 +29,39 @@ public class ExampleProjectStack extends Stack {
     public ExampleProjectStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is: " + s);
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "ExampleProjectQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        GraphqlApi graphqlApi = GraphqlApi.Builder.create(this, "TSPAPI")
+                .name("TSP")
+                .schema(SchemaFile
+                .fromAsset(System.getProperty("user.dir") + "/src/main/java/com/myorg/appsync/schema.graphql"))
+                .build();
+
+        Table table = Table.Builder.create(this, "tsp-ddb-table")
+                .partitionKey(Attribute.builder().name("id").type(AttributeType.STRING).build())
+                .readCapacity(1)
+                .writeCapacity(1)
+                .removalPolicy(RemovalPolicy.DESTROY).build();
+
+        Bucket bucket = Bucket.Builder.create(this, "tsp-s3-bucket")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+
+        DynamoDbDataSource ddb = graphqlApi
+                .addDynamoDbDataSource("tsp-data-source", table);
+
+        CfnUserPool cfnUserPool = CfnUserPool.Builder
+                .create(this, "tsp-user-pool")
+                .userPoolName("tspUserPool152638")
+                .build();
+
+        CfnUserPoolGroup cfnUserPoolGroup = CfnUserPoolGroup.Builder
+                .create(this, "tsp-user-pool-group")
+                .userPoolId(cfnUserPool.getRef())
+                .description("group1")
+                .groupName("group1")
+                .build();
     }
 }
